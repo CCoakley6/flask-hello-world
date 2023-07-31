0a4028d1964442de5ad1e6f9bc62e29b671a1b72
Collin Coakley <coakleyc@colorado.edu>
2023-07-31 01:02:48 +0000

    added db_select

--- app.py
+++ app.py
@@ -14,10 +14,9 @@ def testing():
     return "Database Connection Successful"
 
 @app.route('/db_create')
-def create():
+def creating():
     conn = psycopg2.connect("postgres://lab9postgresql_user:T7yhE4Qqxfpy6ElaKkyUnMV1hXGmoKpf@dpg-cj3g0qd9aq0e0q76mht0-a/lab9postgresql")
     cur = conn.cursor()
-    
     cur.execute('''
         CREATE TABLE IF NOT EXISTS Basketball(
             First varchar(255),
@@ -28,6 +27,41 @@ def create():
             );
     ''')
     conn.commit()
-    
     conn.close()
     return "Basketball table was successfully created."
+
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgres://lab9postgresql_user:T7yhE4Qqxfpy6ElaKkyUnMV1hXGmoKpf@dpg-cj3g0qd9aq0e0q76mht0-a/lab9postgresql")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball table was successfully created."
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgres://lab9postgresql_user:T7yhE4Qqxfpy6ElaKkyUnMV1hXGmoKpf@dpg-cj3g0qd9aq0e0q76mht0-a/lab9postgresql")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+    ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string = ""
+    response_string =+"<table>"
+    for player in records:
+        response_string+="<tr>"
+        for info in player:
+            response_string+="<td>{}</td>".format(info)
+        response_string+="</tr>"
+    response_string+="</table>"
+    return response_string
