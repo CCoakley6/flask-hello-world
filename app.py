c64c28aaa8899748e0a724dc8130bb167bdafd79
Collin Coakley <coakleyc@colorado.edu>
2023-07-31 00:52:56 +0000

    created db_create

--- app.py
+++ app.py
@@ -12,3 +12,22 @@ def testing():
     conn = psycopg2.connect("postgres://lab9postgresql_user:T7yhE4Qqxfpy6ElaKkyUnMV1hXGmoKpf@dpg-cj3g0qd9aq0e0q76mht0-a/lab9postgresql")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def create():
+    conn = psycopg2.connect("postgres://lab9postgresql_user:T7yhE4Qqxfpy6ElaKkyUnMV1hXGmoKpf@dpg-cj3g0qd9aq0e0q76mht0-a/lab9postgresql")
+    cur = conn.cursor()
+    
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit()
+    
+    conn.close()
+    return "Basketball table was successfully created."
